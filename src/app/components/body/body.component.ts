e13776f607d5562a027f03d5b5c0f4c3f42f1842
Dora2021Chen <Dora2021.Chen@outlook.com>
2021-08-08 22:41:04 -0600

    display goods according to their number

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -8,25 +8,29 @@ import { Good } from 'src/app/Good';
 })
 export class BodyComponent implements OnInit {
   goods = [
-    new Good(1, '菜鸟教程'),
-    new Good(2, 'Google'),
-    new Good(3, 'Taobao'),
-    new Good(4, 'Facebook')
+    new Good(1, 'apple'),
+    new Good(2, 'banana'),
+    new Good(3, 'tomato'),
+    new Good(4, 'strawberry'),
+    new Good(5, 'blueberry')
   ];
 
   colNumber: number = 3;
 
-  rows: number[] = [];
-  cols: number[] = [];
+  goodsMatrix: number[][] = [];
   constructor() { }
 
   ngOnInit(): void {
-    for (let i = 0; i < this.colNumber; i++) {
-      this.cols.push(i);
-    }
-
-    for (let i = 0; i < Math.ceil(this.goods.length / this.colNumber); i++) {
-      this.rows.push(i);
+    const rowNumber = Math.ceil(this.goods.length / this.colNumber);
+    for (let row = 0; row < rowNumber; row++) {
+      var rows: number[] = [];
+      for (let col = 0; col < this.colNumber; col++) {
+        var index = row * this.colNumber + col;
+        if (index < this.goods.length) {
+          rows.push(index);
+        }
+      }
+      this.goodsMatrix.push(rows)
     }
   }
 
