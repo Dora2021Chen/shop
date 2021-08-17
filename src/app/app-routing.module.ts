78a4e8430bcd924c88b13571f5731322d0f2dd8d
Dora2021Chen <Dora2021.Chen@outlook.com>
2021-08-17 10:47:31 -0600

    route works now.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'all_goods', component: BodyComponent },
+  { path: 'all_users', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
